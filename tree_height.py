c24d1d0c016013d2a4a1b5f20c8418425a14934c
karsskarsskarss <ajkarthurs@gmail.com>
2023-03-05 19:45:24 +0200

    upd4

--- tree_height.py
+++ tree_height.py
@@ -31,7 +31,7 @@ def main():
         file.close()
         print(compute_height(skaits, dati))
 
-
+#2.versija runo un paskaties main pec tam!!!!!
 sys.setrecursionlimit(10**7)
 threading.stack_size(2**27)
 threading.Thread(target=main).start()
