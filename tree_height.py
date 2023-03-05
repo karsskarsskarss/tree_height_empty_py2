a584c396534e9f4029f2f1caf19a5ddf4a2ecd15
karsskarsskarss <ajkarthurs@gmail.com>
2023-03-05 19:53:24 +0200

    upd5

--- tree_height.py
+++ tree_height.py
@@ -1,35 +1,24 @@
 import sys
 import threading
-import numpy
+
 
 def compute_height(n, parents):
     # Write this function
     max_height = 0
     for x in range(n):
-        dzilums = 0
-        id = x
-        while id != -1:
-            dzilums = dzilums + 1
+       height = 0
+       id = x
+       while id != -1:
+            height += 1
             id = parents[id]
-        max_height = max(max_height, dzilums)
-
+       max_height = max(max_height, height)
     return max_height
 
 def main():
-    text = str(input())
-    if "I" in text:
-        skaits = int(input())
-        dati = list(map(int, input().split()))
-        print(compute_height(skaits, dati))
-
-    if "F" in text:
-        name = str(input())
-        name = "test/" + str(name)
-        file = open(name,'r')
-        skaits = int(file.readline())
-        dati = list(map(int, file.readline().split()))
-        file.close()
-        print(compute_height(skaits, dati))
+    n = int(input())
+    parents = list(map(int, input().split()))
+    
+    print(compute_height(n, parents))
 
 #2.versija runo un paskaties main pec tam!!!!!
 sys.setrecursionlimit(10**7)
